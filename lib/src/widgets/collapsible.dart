6aa70fc1f360c7ca398620e8133ef62499a1212c
glisco <glisco03@web.de>
2025-07-21 23:04:38 +0200

    add optional vertical rule to collapsibles

--- lib/src/widgets/collapsible.dart
+++ lib/src/widgets/collapsible.dart
@@ -1,19 +1,27 @@
+import '../animation/easings.dart';
 import '../core/cursors.dart';
 import '../core/math.dart';
 import '../framework/proxy.dart';
 import '../framework/widget.dart';
+import 'animated_widgets.dart';
 import 'basic.dart';
 import 'flex.dart';
 import 'icon.dart';
+import 'stack.dart';
+import 'theme.dart';
 
-class Collapsible extends StatelessWidget {
+class Collapsible extends StatefulWidget {
+  final bool showVerticalRule;
+
   final bool collapsed;
   final void Function(bool nowCollapsed) onToggled;
+
   final Widget title;
   final Widget content;
 
   const Collapsible({
     super.key,
+    this.showVerticalRule = false,
     required this.collapsed,
     required this.onToggled,
     required this.title,
@@ -21,23 +29,64 @@ class Collapsible extends StatelessWidget {
   });
 
   @override
+  WidgetState<Collapsible> createState() => _CollapsibleState();
+}
+
+class _CollapsibleState extends WidgetState<Collapsible> {
+  bool hovered = false;
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Actions.click(
-              cursorStyle: CursorStyle.hand,
-              onClick: () => onToggled(!collapsed),
-              child: Icon(icon: collapsed ? Icons.arrow_right : Icons.arrow_drop_down, size: 20),
-            ),
-            Sized(height: 24, child: title),
-          ],
+        MouseArea(
+          enterCallback: widget.showVerticalRule
+              ? () => setState(() {
+                  hovered = true;
+                })
+              : null,
+          exitCallback: widget.showVerticalRule
+              ? () => setState(() {
+                  hovered = false;
+                })
+              : null,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Actions.click(
+                cursorStyle: CursorStyle.hand,
+                onClick: () => widget.onToggled(!widget.collapsed),
+                child: Icon(icon: widget.collapsed ? Icons.arrow_right : Icons.arrow_drop_down, size: 20),
+              ),
+              Sized(height: 24, child: widget.title),
+            ],
+          ),
         ),
         Visibility(
-          visible: !collapsed,
-          child: Padding(insets: const Insets(left: 20), child: content),
+          visible: !widget.collapsed,
+          child: Stack(
+            children: [
+              if (widget.showVerticalRule)
+                Align(
+                  alignment: Alignment.left,
+                  child: Padding(
+                    insets: const Insets(left: 9),
+                    child: Sized(
+                      width: 2,
+                      height: double.infinity,
+                      child: AnimatedPanel(
+                        duration: const Duration(milliseconds: 125),
+                        easing: Easing.inOutExpo,
+                        color: hovered ? BraidTheme.of(context).elementColor : BraidTheme.of(context).elevatedColor,
+                      ),
+                    ),
+                  ),
+                ),
+              StackBase(
+                child: Padding(insets: const Insets(left: 20), child: widget.content),
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -45,13 +94,17 @@ class Collapsible extends StatelessWidget {
 }
 
 class LazyCollapsible extends StatefulWidget {
+  final bool showVerticalRule;
+
   final bool collapsed;
   final void Function(bool nowCollapsed) onToggled;
+
   final Widget title;
   final Widget content;
 
   LazyCollapsible({
     super.key,
+    this.showVerticalRule = false,
     required this.collapsed,
     required this.onToggled,
     required this.title,
@@ -72,6 +125,7 @@ class _LazyCollapsibleState extends WidgetState<LazyCollapsible> {
     }
 
     return Collapsible(
+      showVerticalRule: widget.showVerticalRule,
       collapsed: widget.collapsed,
       onToggled: widget.onToggled,
       title: widget.title,
